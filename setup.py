df82403f199e578d61b0323d75662ae065828a00
Wajdy <wajdy1011000@gmail.com>
2018-12-19 07:09:46 +0200

    Add LICENSE and setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+	name='modeler',
+	version='0.0.1',
+	packages=['modeler'],
+	url='https://github.com/Wajdy759/modeler',
+	license='mit',
+	author='Wajdy',
+	author_email='itswajdy@gmail.com',
+	description='Reconstruct 3D models of objects from multiple images'
+)
